e06dcd5d7d913332c4745fcac84e2fcf965cc389
OpenSourceZombie <shirbiny@onenetbeyond.org>
2013-12-06 17:37:04 -0500

    faster iterations , removed quick sort

--- Sorty.php
+++ Sorty.php
@@ -1,5 +1,5 @@
 <?php
-/*Testing*/
+
 //$array=array(10,3,4,6,33,1,55,12,67,1,0,-1,-2,-3);
 //var_dump($array);
 //$sort=new Sorty($array,"quick",true);
@@ -37,19 +37,24 @@ switch ($this->algorthim){
 
 function insertionSort()
 {
-	if ($this->show)echo "Insertion Sorting<br/>";
+
+	$show=$this->show;
+	if ($show)echo "Insertion Sorting<br/>";
 	$elements=$this->array;	
 	for($i=1; $i<count($elements); $i++) {
-        // Always compare the bit we're looking at to its next left neighbor, unless we're at the leftmost side of the array
         for ($j=$i-1; $j>=0; $j--) {
-			if($this->show)$this->showElements($elements);
+			if($show)$this->showElements($elements);
             //sleep(1); // this is just so you can see the work in progress
              //$comparisons++;
             // If the left neighbor is actually bigger than our current bit, let's swap them
             if ($elements[$j]>$elements[$j+1]){
  			// echo "$elements[$j] is bigger than ".$elements[$j+1]."\n";
                 //XOR swpping
-				$elements[$j]^=$elements[$j+1]^=$elements[$j]^=$elements[$j+1];
+				$tmp=0;
+				$tmp=$elements[$j];
+				$elements[$j]=$elements[$j+1];
+				$elements[$j+1]=$tmp;
+ 
 //              $swappings++; // count how often we're swapping things around
 
           	} else 
@@ -62,27 +67,32 @@ return $elements;
 
 function bubbleSort()
 {
-	if($this->show)echo "Bubble Sorting <br />";
+	$show=$this->show;
+	if($show)echo "Bubble Sorting <br />";
 	$elements=$this->array;
     $size = count($elements);
     for ($i=0; $i<$size; $i++) {
         for ($j=0; $j<$size-1-$i; $j++) {
-			if($this->show)$this->showElements($elements);
+			if($show)$this->showElements($elements);
             if ($elements[$j+1] < $elements[$j]) {
 			//XOR swpping
 		//	echo "<br /> $j";
-			$elements[$j]^=$elements[$j+1]^=$elements[$j]^=$elements[$j+1];
+			$tmp=0;
+			$tmp=$elements[$j];
+			$elements[$j]=$elements[$j+1];
+			$elements[$j+1]=$tmp;
             }
         }
     }
     return $elements;
 }
-
+/*
 function quickSort() 
 {
-//	if($this->show) echo "Quick Sorting<br />";
+//	$show=$this->show;
+	if($show) echo "Quick Sorting<br />";
 	$elements=$this->array;
-	if($this->show)$this->showElements($elements);
+//	if($show)$this->showElements($elements);
 	if( count( $elements ) < 2 ) {
         return $elements;
     }
@@ -98,12 +108,13 @@ function quickSort()
     }
     return array_merge($this->quicksort($left), array($pivot_key => $pivot), $this->quicksort($right));
 }  
-
+*/
 function selectionSort(){
-	if ($this->show)echo "Selection Sorting <br />";
+	$show=$this->show;
+	if ($show)echo "Selection Sorting <br />";
 	$elements=$this->array;
 	for ($i = 0; $i < count($elements); ++$i) {
-		if($this->show)$this->showElements($elements);
+		if($show)$this->showElements($elements);
 		$min = null;
 		$minKey = null;
 		for($j = $i; $j < count($elements); ++$j) {
@@ -121,8 +132,9 @@ return $elements;
 
 function shellSort()
 {
+	$show=$this->show;
 	$elements=$this->array;
-	if ($this->show)echo "Shell Sorting<br />";
+	if ($show)echo "Shell Sorting<br />";
 	$length=count($elements);
 	$k=0;
 	$gap[0]=(int) ($length / 2);
@@ -140,7 +152,7 @@ function shellSort()
 			 while($p>=0 && $temp<$elements[$p]) {
 				$elements[$p+$step]=$elements[$p];
 				$p=$p-$step;
-				if ($this->show)$this->showElements($elements);
+				if ($show)$this->showElements($elements);
 			 }//end while
 			 $elements[$p+$step]=$temp;
 		 }//endfor j
